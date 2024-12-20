c1ce25f5523224795a04fcced4db0a1b669eb181
Meltistry <162095342+Meltistry@users.noreply.github.com>
2024-12-21 02:28:50 +0700

    Sudoku 1.1
    Added multiple other features.

---
+++ sudoku/SudokuConstants.java
@@ -0,0 +1,22 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #16
+ * 1 - 5026231145 - Abrorus Shobah
+ * 2 - 5026231076 - James Melvin Chandra
+ */
+
+
+
+
+ package sudoku;
+ /**
+  * Define the named constants used in many classes.
+  */
+ public class SudokuConstants {
+    /** Size of the board */
+    public static final int GRID_SIZE = 9;
+    /** Size of sub-grid of the board */
+    public static final int SUBGRID_SIZE = 3;
+ }
